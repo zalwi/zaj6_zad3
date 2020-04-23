92f6d2b3eaae08d1e38cd8c60756cc23836ea18f
Kamil Zalwert <kamilz89@gmail.com>
2020-04-23 16:01:47 +0200

    zaj 6 zad 3; wyswietlanie + poprawka dla ostatniej iteracji

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int sum = 0;
-        for(int i=0;i<100;i++){
-            if((i%2)==0) sum+=i;
+        for(int i=0;i<=100;i++){
+            if((i%2)==0){
+                sum+=i;
+                System.out.println(i);
+            }
         }
         System.out.println("Suma liczb parzystych 0-100: " + sum);
     }
